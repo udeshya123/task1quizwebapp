41c43f0c23a09ab2c098fc68bbb3d9b6419dc6e7
gyanendra singh <67867739+udeshya123@users.noreply.github.com>
2022-02-20 18:07:54 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,128 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not an OOPS concept?",
+    answer: "Exception",
+    options: [
+      "Encapsulation",
+      "Polymorphism",
+      "Exception",
+      "Abstraction"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which among the following feature is not in the general definition of OOPS?",
+    answer: "Duplicate or Redundant Data",
+    options: [
+      "Modularity",
+      "Efficient Code",
+      "Code reusability",
+      "Duplicate or Redundant Data"
+    
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which feature of OOPS described the reusability of code?",
+    answer: "Inheritance",
+    options: [
+      "Abstraction",
+      "Encapsulation",
+      "Polymorphism",
+      "Inheritance"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which feature of OOPS derives the class from another class?",
+    answer: "Inheritance",
+    options: [
+      "Inheritance",
+      "Data hiding",
+      "Encapsulation",
+      "Polymorphism"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A single program of OOPS contains ___ classes?",
+    answer: "Any number",
+    options: [
+      "Only 1",
+      "Only 999",
+      "Only 100",
+      "Any number"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which two features of object-oriented programming are the same?",
+    answer: "Encapsulation and Abstraction",
+    options: [
+      "Abstraction and Polymorphism features are the same",
+      "Inheritance and Encapsulation features are the same",
+      "Encapsulation and Polymorphism features are the same",
+      "Encapsulation and Abstraction"
+    ]
+  },
+   {
+    numb: 7,
+    question: "Which header file is required by the C++ programming language to use the OOPS concept?",
+    answer: "We can easily use the OOPS concepts in c++ programs without using any header file.",
+    options: [
+      "stdio.h",
+      "iostream.h",
+      "stdlib.h",
+      "We can easily use the OOPS concepts in c++ programs without using any header file."
+    ]
+  },
+   {
+    numb: 8,
+    question: "Which of the following definition is incorrect for polymorphism?",
+    answer: "It always increases the overhead of function definition",
+    options: [
+      "Polymorphism helps in redefining the same functionality",
+      "Polymorphism concept is the feature of object-oriented programming (OOP)",
+      "It always increases the overhead of function definition",
+      "Ease in the readability of the program"
+    ]
+  },
+   {
+    numb: 9,
+    question: "Which of the following OOP concept is not true for the C++ programming language?",
+    answer: "C++ Program must contain at least one class",
+    options: [
+      "A class must have member functions",
+      "C++ Program can be easily written without the use of classes",
+      "At least one instance should be declared within the C++ program",
+      "C++ Program must contain at least one class"
+    ]
+  },
+   {
+    numb: 10,
+    question: "Which of the following OOP concept binds the code and data together and keeps them secure from the outside world?",
+    answer: "Encapsulation",
+    options: [
+      "Polymorphism",
+      "Inheritance",
+      "Abstraction",
+      "Encapsulation"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
